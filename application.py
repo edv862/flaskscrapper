e9e88171fb45f63e1ea2bd55a2b824f1ab4408b4
edv862 <evalderrama862@gmail.com>
2017-09-06 07:26:01 -0400

    Frequent getter

--- application.py
+++ application.py
@@ -27,6 +27,31 @@ def getInfo():
     return csvstring
 
 
+def getFrequentInfo():
+    db = dbHelper()
+    csvstring = """name,make,model,colour,capacity,image,sku,url,category,subcategory,grade,sellprice,buyprice,voucherprice
+"""
+    con = db.connect()
+    cur = con.cursor()
+    products = cur.execute(
+        """SELECT p.name, p.make, p.model, p.colour, p.capacity, p.img, p.sku, url.url_dir, cat.name, scat.name, grade.type, price.sell_price, price.buy_price, price.voucher_price
+            FROM product as p
+            LEFT JOIN url ON p.url=url.id
+            LEFT JOIN category AS cat ON p.category=cat.id
+            LEFT JOIN subcategory AS scat ON p.sub_category=scat.id
+            LEFT JOIN grade ON p.grade=grade.id
+            LEFT JOIN price ON p.price=price.id
+            WHERE p.frequent=1
+            """)
+
+    while products > 0:
+        aux = cur.fetchone()
+        csvstring += (str(aux)[1:-1] + '\n').replace('\'', '"')
+        products -= 1
+
+    return csvstring
+
+
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
@@ -36,6 +61,15 @@ def index():
     csvstring = getInfo()
     response = make_response(csvstring)
     response.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    response.headers["Content-type"] = "text/csv"
+    return response
+
+
+@application.route('/frequent')
+def getFrequent():
+    csvstring = getFrequentInfo()
+    response = make_response(csvstring)
+    response.headers["Content-Disposition"] = "attachment; filename=export-frequent.csv"
     response.headers["Content-type"] = "text/csv"
     return response
 
